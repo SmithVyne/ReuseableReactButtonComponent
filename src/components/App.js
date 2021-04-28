58ee149859c3b7ccd06976fc7bd05806b736856d
SmithVyne <smithnkereuwem2@gmail.com>
2021-04-28 11:24:10 +0300

    First working version

--- src/components/App.js
+++ src/components/App.js
@@ -1,52 +1,16 @@
-import React, {useState} from 'react';
+// import React, {useState} from 'react';
 import '../App.css';
 import Button from "./Button";
-import UpdateButton from "./UpdateButton";
+// import UpdateButton from "./UpdateButton";
 
-const defaultStyle = {
-  width: '81px',
-  height: '36px',
-  fontFamily: 'cursive',
-  fontStyle: 'normal',
-  fontWeight: 'bold',
-  fontSize: '14px',
-  lineHeight: '20px',
-  borderRadius: '4px',
-  border: 'none'
-}
 
-const outlineStyle = {
-  border: '1px solid #3D5AFE',
-  color: '#3D5AFE'
-}
-
-const textOnlyStyle = {
-  border: "none",
-  backgroundColor: "white"
-}
 
 function App() {
-  const [buttonText, updateButtonText] =  useState("Default");
-  // const [buttonProps, setButtonProps] = useState({});
-  const [buttonStyle, setButtonStyle] = useState(defaultStyle);
-
-  const handleButtonStyle = ({target: { value }}) => {
-    // console.log(value)
-    if (value === "Default") {
-      setButtonStyle(defaultStyle);
-    } else if(value === "Outline") {
-      setButtonStyle(state => ({...state, ...outlineStyle}))
-    } else if(value === "Text") {
-      setButtonStyle(state => ({...state, ...textOnlyStyle}))
-    }
-  }
-
   return (
     <div className="App">
       <h1 id="page-title">Buttons</h1>
       <main>
-        <Button buttonStyle={buttonStyle} buttonText={buttonText} />
-        <UpdateButton onChange={handleButtonStyle}  />
+        <Button />
       </main>
     </div>
   );
--- src/components/Button.js
+++ src/components/Button.js
@@ -1,8 +1,57 @@
+import {useState, useEffect, useRef} from 'react' 
 
-export default ({buttonStyle, buttonText, ...attributes}) => {
+const variants = {
+    default: {
+        width: '81px',
+        height: '36px',
+        fontFamily: 'cursive',
+        fontStyle: 'normal',
+        fontWeight: 'bold',
+        fontSize: '14px',
+        lineHeight: '20px',
+        borderRadius: '4px',
+        border: 'none',
+        outline: 'none',
+      },
+    outline: {
+        border: '1px solid #3D5AFE'
+    },
+    text: {
+        border: "none",
+        backgroundColor: "white"
+    }
+}
+  
+
+const Button = ({variant="default", ...attributes}) => {
+    const [buttonText, setButtonText] = useState("Default");
+    const [buttonStyle, setButtonStyle] = useState(variants['default']);
+    const buttonRef = useRef();
+    
+    useEffect(() => {
+        setButtonStyle( state =>({...state, ...variants[variant]}))
+        if (variant === 'default') {
+            buttonRef.current.classList.remove("text-blue");
+        }
+        else {
+            buttonRef.current.classList.add("text-blue");
+        }
+    }, [variant])
+
+    const handleHover = ({type, target}) => {
+        if(type === 'mouseover') {
+            target.classList.add(`${variant}_hover`)
+        } else if(type === 'mouseout') {
+            target.classList.remove(`${variant}_hover`)
+        }
+    }
+    
     return (
-        <button style={buttonStyle} {...attributes} > 
+        <button className="" ref={buttonRef} onMouseOver={handleHover} onMouseOut={handleHover} style={buttonStyle}  {...attributes} > 
             {buttonText}
         </button>
     )
 }
+
+
+export default Button;
