9adb69e4ce79297a5a44a3041695916a184a91d6
SmithVyne <smithnkereuwem2@gmail.com>
2021-04-29 15:17:47 +0300

    css in js styles work

--- src/components/Button.js
+++ src/components/Button.js
@@ -12,42 +12,44 @@ const variants = {
         borderRadius: '4px',
         border: 'none',
         outline: 'none',
-      },
+        backgroundColor: '#E0E0E0',
+        color: '#3F3F3F'
+    },
     outline: {
-        border: '1px solid #3D5AFE'
+        border: '1px solid #3D5AFE',
+        backgroundColor: 'transparent',
+        color: "#3D5AFE"
     },
     text: {
         border: "none",
-        backgroundColor: "white"
+        backgroundColor: 'transparent',
+        color: "#3D5AFE"
     }
 }
   
 
 const Button = ({variant="default", ...attributes}) => {
+    console.log(variant)
     const [buttonText, setButtonText] = useState("Default");
-    const [buttonStyle, setButtonStyle] = useState(variants['default']);
+    const [buttonStyle, setButtonStyle] = useState({});
+    
     const buttonRef = useRef();
     
     useEffect(() => {
-        setButtonStyle( state =>({...state, ...variants[variant]}))
-        if (variant === 'default') {
-            buttonRef.current.classList.remove("text-blue");
-        }
-        else {
-            buttonRef.current.classList.add("text-blue");
-        }
+        setButtonStyle({...variants['default'], ...variants[variant]})
     }, [variant])
 
     const handleHover = ({type, target}) => {
-        if(type === 'mouseover') {
-            target.classList.add(`${variant}_hover`)
-        } else if(type === 'mouseout') {
-            target.classList.remove(`${variant}_hover`)
-        }
+        // if(type === 'mouseover') {
+        //     target.classList.add(`${variant}_hover`)
+        // } else if(type === 'mouseout') {
+        //     target.classList.remove(`${variant}_hover`)
+        // }
+        target.classList.toggle(`${variant}_hover`);
     }
     
     return (
-        <button className="" ref={buttonRef} onMouseOver={handleHover} onMouseOut={handleHover} style={buttonStyle}  {...attributes} > 
+        <button className="" onMouseOver={handleHover} onMouseOut={handleHover} ref={buttonRef} style={buttonStyle} {...attributes} > 
             {buttonText}
         </button>
     )
