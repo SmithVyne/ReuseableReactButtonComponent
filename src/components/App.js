29c3800a6a5f5e087b822786501d84a9b0437681
SmithVyne <smithnkereuwem2@gmail.com>
2021-05-03 04:02:15 +0300

    Add disable button and disable shadow buttons

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,21 @@
-// import React, {useState} from 'react';
+import React, {useState} from 'react';
 import '../App.css';
 import Button from "./Button";
-// import UpdateButton from "./UpdateButton";
+import UpdateButton from "./UpdateButton";
 
 
 
 function App() {
+  const [buttonProps, setButtonProps] = useState({})
+  
+
   return (
     <div className="App">
       <h1 id="page-title">Buttons</h1>
       <main>
-        <Button variant='text' startIcon='face' endIcon='local_grocery_store' color='primary' />
+        {/* <Button variant='text' startIcon='face' endIcon='local_grocery_store' color='primary' /> */}
+        <Button {...buttonProps} />
+        <UpdateButton buttonProps={buttonProps} setButtonProps={setButtonProps} />
       </main>
     </div>
   );
--- src/components/UpdateButton.js
+++ src/components/UpdateButton.js
@@ -1,14 +1,34 @@
 import React from 'react'
 
-export default function UpdateButton({onChange}) {
+function UpdateButton({buttonProps, setButtonProps}) {
+    const {disabled, disableShadow, variant} = buttonProps;
+    const DisInstruction = disabled ? 'Enable' : 'Disable';
+    const ShadInstruction = disableShadow ? 'Add' : 'Remove';
     return (
-        <div>
-            <p>Options</p>
-            <select onChange={onChange}>
-                <option selected>Default</option>
-                <option>Outline</option>
-                <option>Text</option>
-            </select>
-        </div>
+        <>
+            <div>
+                <label>Variants</label>
+                <select onChange={({target}) => setButtonProps(props => ({...props, variant: target.value})) } >
+                    <option value="default">Default</option>
+                    <option value="outline">Outline</option>
+                    <option value="text">Text</option>
+                </select>
+
+                <label>Sizes</label>
+                <select onChange={({target}) => setButtonProps(props => ({...props, size: target.value})) } >
+                    <option value="sm">Small</option>
+                    <option selected value="md">Medium</option>
+                    <option value="lg">Large</option>
+                </select>
+            </div>
+
+            <div>
+                {variant === 'default' && <button>{ShadInstruction} Shadow</button>}
+                <button>{DisInstruction} Button</button>
+            </div>
+        </>
     )
 }
+
+
+export default UpdateButton;
