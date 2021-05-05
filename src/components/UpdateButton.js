71e28cf7b4ffba7f2cf6e9f8bf69ffb81918ea94
SmithVyne <smithnkereuwem2@gmail.com>
2021-05-05 15:42:27 +0300

    Update onchange and onclick for shadow, disable, size, and variants form elements

--- src/components/UpdateButton.js
+++ src/components/UpdateButton.js
@@ -6,32 +6,39 @@ function UpdateButton({buttonProps, setButtonProps}) {
     const ShadInstruction = disableShadow ? 'Add' : 'Remove';
 
     const buttonColors = (boolean) => ({backgroundColor: boolean ? '#455A64' : '#2962FF'});
+    const variantsAndSizes = ({name, value}) => setButtonProps(props => ({...props, [name]: value}))
+    const shadowAndDisable = (name, boolValue) => setButtonProps(props => ({...props, [name]: !boolValue}));
 
     return (
         <>
             <div>
                 <label>Variants</label>
-                <select onChange={({target}) => setButtonProps(props => ({...props, variant: target.value})) } >
+                <select name="variant" onChange={({target}) => variantsAndSizes(target) } >
                     <option value="default">Default</option>
                     <option value="outline">Outline</option>
                     <option value="text">Text</option>
                 </select>
 
                 <label>Sizes</label>
-                <select defaultValue="md" onChange={({target}) => setButtonProps(props => ({...props, size: target.value})) } >
+                <select name="size" defaultValue="md" onChange={({target}) => variantsAndSizes(target) } >
                     <option value="sm">Small</option>
                     <option value="md">Medium</option>
                     <option value="lg">Large</option>
                 </select>
             </div>
             
+
             <div className="grid-group">
                 {variant === 'default' && 
-                    <button style={buttonColors(disableShadow)} onClick={() => setButtonProps(props => ({...props, disableShadow: !disableShadow}))} className="disableBtns">
+                    <button style={buttonColors(disableShadow)} onClick={() => shadowAndDisable('disableShadow', disableShadow)} className="disableBtns">
                         {ShadInstruction} Shadow
                     </button>
                 }
-                <button style={buttonColors(disabled)} onClick={() => setButtonProps(props => ({...props, disabled: !disabled}))} className="disableBtns">{DisInstruction} Button</button>
+                <button style={buttonColors(disabled)} onClick={() => shadowAndDisable('disabled', disabled)} className="disableBtns">{DisInstruction} Button</button>
+            </div>
+
+            <div>
+                <button></button>
             </div>
         </>
     )
